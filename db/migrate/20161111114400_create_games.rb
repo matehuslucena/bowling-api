3ae2ddab4d911485489fcf609c068e54ec893cf9
Matheus Lucena <matheuslucena@gmail.com>
2016-11-11 10:02:19 -0200

    correct migration

--- db/migrate/20161111114400_create_games.rb
+++ db/migrate/20161111114400_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
       t.integer :total_score
-      t.frame :references
+      t.references :frame
 
       t.timestamps
     end
