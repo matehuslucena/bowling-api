9e423387f7756f3ad8c2c54657d3c5b6d73b1770
Matheus Lucena <matheuslucena@gmail.com>
2016-11-11 11:10:02 -0200

    added default value for total_score

--- db/migrate/20161111114340_create_frames.rb
+++ db/migrate/20161111114340_create_frames.rb
@@ -1,7 +1,8 @@
 class CreateFrames < ActiveRecord::Migration[5.0]
   def change
     create_table :frames do |t|
-      t.integer :total_score
+      t.integer :total_score, default: 0
+      t.references :game
 
       t.timestamps
     end
--- db/migrate/20161111114400_create_games.rb
+++ db/migrate/20161111114400_create_games.rb
@@ -1,8 +1,7 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
-      t.integer :total_score
-      t.references :frame
+      t.integer :total_score, default: 0
 
       t.timestamps
     end
