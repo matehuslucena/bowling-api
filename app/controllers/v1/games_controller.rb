6871adb568eecc19eda86602f9f6a3b1f47ff1c2
Matheus Lucena <matheuslucena@gmail.com>
2016-11-13 19:06:04 -0200

    implement controller to manage the game

--- app/controllers/v1/games_controller.rb
+++ app/controllers/v1/games_controller.rb
@@ -1,12 +1,27 @@
 class V1::GamesController < ApplicationController
   include ActionController::Serialization
 
-  def index
-  end
+  before_action :set_game, only: [:update, :show]
 
   def create
     game = Game.create
 
     render json: game
+  end
+
+  def update
+    @game.check_played(params[:knocked_pins].to_i)
+
+    render json: @game
+  end
+
+  def show
+    render json: @game
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 end
