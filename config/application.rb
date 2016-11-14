ed872ccd305d267c957a4f733ad5771d507e2a4f
Matheus Lucena <matheuslucena@gmail.com>
2016-11-14 14:25:16 -0200

    set locale

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module BowlingApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    I18n.default_locale = :en
   end
 end
