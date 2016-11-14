578248d45ab40407b96896293c9724db38368090
Matheus Lucena <matheuslucena@gmail.com>
2016-11-14 16:31:24 -0200

    added methods to improve understand

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,6 +1,18 @@
 class Frame < ApplicationRecord
-  def join_score knocked_pins
+  def increment_score knocked_pins
     self.score += knocked_pins
     self.save
+  end
+
+  def finalize
+    self.update(is_active: false)
+  end
+
+  def set_strike
+    self.update(result: 'strike')
+  end
+
+  def set_spare
+    self.update(result: 'spare')
   end
 end
